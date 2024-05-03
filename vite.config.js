8f8d482faeabdd9d846183b32d77271901aef5d9
MathisHumbert <mathishumbert71@gmail.com>
2024-05-03 19:14:25 +0200

    fix build

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,21 @@ import { glslify } from 'vite-plugin-glslify';
 
 export default {
   plugins: [glslify()],
+  build: {
+    outDir: 'dist',
+    ssrManifest: true,
+    emptyOutDir: true,
+    minify: 'esbuild',
+    rollupOptions: {
+      input: {
+        main_js: 'src/client/index.js',
+        main_css: 'src/styles/index.scss',
+      },
+      output: {
+        entryFileNames: 'main.js',
+        assetFileNames: ({ name }) =>
+          name === 'main_css.css' ? 'main.css' : '[name].[ext]',
+      },
+    },
+  },
 };
